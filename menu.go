225287a75d1dc5efb3ce1eaef5fe658a89eb7b74
agastya gaur <agastyagaur17@gmail.com>
2024-03-13 19:46:33 +0530

    Add order modification functionality

--- menu.go
+++ menu.go
@@ -5,34 +5,33 @@ import (
 	"strings"
 )
 
-type Menu struct{
+type Menu struct {
 	itemNo    uint
 	itemName  string
 	itemPrice float64
-
 }
 
 var menu = []Menu{
-	{1, "Chole Bhature", 100},
-	{2, "Rajma Chawal", 80},
-	{3, "Paneer Tikka", 150},
-	{4, "Butter Chicken", 200},
-	{5, "Chicken Biryani", 180},
-	{6, "Mutton Rogan Josh", 250},
-	{7, "Fish Curry", 200},
-	{8, "Fish Fry", 150},
-	{9, "Papad", 10},
-	{10, "Raita", 20},
-	{11, "Lassi", 30},
-	{12, "Cold Drink", 40},
-	{13, "Water Bottle", 20},
-
+	//itemNo  itemName   itemPrice
+	{1, "Adrakh Chai", 20},
+	{2, "Filter Coffee", 25},
+	{3, "Chhas", 35.50},
+	{4, "Lassi", 30},
+	{5, "Mango Lassi", 60},
+	{6, "Kadi Chaawal", 50},
+	{7, "Chhole Bhature", 45},
+	{8, "Khasta Kachori", 30},
+	{9, "Raj Kachori", 30},
+	{10, "Veg. Sandwich", 20},
+	{11, "Veg. Masala Maggi", 60.00},
+	{12, "Samosa", 20},
+	{13, "Cream Roll", 15},
 }
 
-func printMenu(){
+func printMenu() {
 	fmt.Printf("%15s\n", "Menu")
 	fmt.Printf("%s\n", strings.Repeat("-", 35))
-	fmt.Printf("%-7s %6s    %12s\n", "S.No.","Item Name", "Price" )
+	fmt.Printf("%-7s %6s    %12s\n", "S.No.", "Price", "Item Name")
 	fmt.Printf("%s\n", strings.Repeat("-", 35))
 
 	for _, element := range menu {
